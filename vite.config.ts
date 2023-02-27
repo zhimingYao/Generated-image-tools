d7896fdd2c431bc1359a6b9668d39d4673c4a31d
yaozhiming <1243204919@qq.com>
2023-02-27 17:03:56 +0800

    main

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import dns from "dns";
+dns.setDefaultResultOrder("verbatim"); //禁用这个重新排序的行为
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+export default defineConfig(({ command, mode }) => {
+  return {
+    base: "./",
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  };
+});

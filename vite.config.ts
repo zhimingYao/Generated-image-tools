86a57cf6689c26e7800a9c886019f0ad7a85fb9b
yaozhiming <1243204919@qq.com>
2023-02-27 17:39:18 +0800

    main

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ dns.setDefaultResultOrder("verbatim"); //禁用这个重新排序的行为
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
+  // 根据当前工作目录中的 `mode` 加载 .env 文件
+  // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
+  const env = loadEnv(mode, process.cwd(), "");
   return {
     base: "./",
     plugins: [vue()],
